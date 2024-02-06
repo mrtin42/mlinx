5086d279b4d45e015730ea18a8468844e9604747
martin <martin@mbfrias.co.uk>
2024-02-06 12:17:40 +0000

    whoopds padding

--- components/dashnav.tsx
+++ components/dashnav.tsx
@@ -5,17 +5,17 @@ export default function DashNav({ active }: { active: string }) {
     return (
         <div className="fixed border-b border-zinc-100-/10 bg-black/50 backdrop-blur-sm flex flex-row items-center justify-center">
             <div className="flex flex-row items-center justify-between p-3">
-                <div className={`${active === "dashboard" ? "text-cyan-950" : "text-gray-300"} text-sm italic`}>
+                <div className={`${active === "dashboard" ? "text-cyan-950" : "text-gray-300"} text-sm italic m-2 mr-4`}>
                     <Link href="/dashboard">
                         Dashboard
                     </Link>
                 </div>
-                <div className={`${active === "domains" ? "text-cyan-950" : "text-gray-300"} text-sm italic`}>
+                <div className={`${active === "domains" ? "text-cyan-950" : "text-gray-300"} text-sm italic m-2`}>
                     <Link href="/dashboard/domains">
                         Domains
                     </Link>
                 </div>
-                <div className={`${active === "settings" ? "text-cyan-950" : "text-gray-300"} text-sm italic`}>
+                <div className={`${active === "settings" ? "text-cyan-950" : "text-gray-300"} text-sm italic m-2`}>
                     <Link href="/dashboard/settings">
                         Settings
                     </Link>
