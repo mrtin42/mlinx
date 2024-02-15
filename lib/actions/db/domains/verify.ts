3edebcb85b016b4d1e01a6a4f843a7edaa5a45ec
martin <martin@tub3.uk>
2024-02-15 01:14:29 +0000

    removed cloudflare workaround due to connections getting refused

--- lib/actions/db/domains/verify.ts
+++ lib/actions/db/domains/verify.ts
@@ -17,49 +17,39 @@ if (process.env.NEXT_PUBLIC_ENV === 'development') {
         print(domains);
         let results = [];
         for (const domain of domains) {
-            const onCloudflare = await dig.resolveNs(domain.domain);
-            print(onCloudflare);
-            if (onCloudflare[0].includes('ns.cloudflare.com') || onCloudflare[0].includes('cloudflare.net')) {
-                const x = await dig.resolve4(domain.domain);
-                const res = await axios.get(`https://${x[0]}/_nslookup/trace`, { headers: { host: domain.domain } });
-                print(res);
-                if (res.data.status) {
-                    results.push({domain: domain.domain, status: true, conn: 'A', addresses: ['This domain is correctly pointing to mlinx\'s servers via Cloudflare\'s proxy.']});
+            
+            switch (!domain.domain.split('.')[2]) {
+                case true: {
+                    try {
+                        const res = await dig.resolve4(domain.domain);
+                        print(res);
+                        // regex pattern to test if the resolved IP address is a Vercel IP address (begins with 76.76.21 and ends with anything between 1 and 255)
+                        if (/^76\.76\.21\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(res[0])) {
+                            results.push({domain: domain.domain, status: true, conn: 'A', addresses: res});
+                        } else {
+                            results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolvedInvalid'});
+                        }
+                    } catch (e) {
+                        results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolverError'});
+                    }
+                    break;
                 }
-            } else {
-                switch (!domain.domain.split('.')[2]) {
-                    case true: {
-                        try {
-                            const res = await dig.resolve4(domain.domain);
-                            print(res);
-                            // regex pattern to test if the resolved IP address is a Vercel IP address (begins with 76.76.21 and ends with anything between 1 and 255)
-                            if (/^76\.76\.21\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(res[0])) {
-                                results.push({domain: domain.domain, status: true, conn: 'A', addresses: res});
-                            } else {
-                                results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolvedInvalid'});
-                            }
-                        } catch (e) {
-                            results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolverError'});
+                case false: {
+                    try {
+                        const res = await dig.resolveCname(domain.domain);
+                        print(res);
+                        if (/^cname\.(vercel-dns|martin-dns)\.com$/.test(res[0])) {
+                            results.push({domain: domain.domain, status: true, conn: 'CNAME', address: res});
+                        } else {
+                            results.push({domain: domain.domain, status: false, conn: 'CNAME', error: 'resolvedInvalid'});
                         }
-                        break;
+                    } catch (e) {
+                        results.push({domain: domain.domain, status: false, conn: 'CNAME', error: 'resolverError'});
                     }
-                    case false: {
-                        try {
-                            const res = await dig.resolveCname(domain.domain);
-                            print(res);
-                            if (/^cname\.(vercel-dns|martin-dns)\.com$/.test(res[0])) {
-                                results.push({domain: domain.domain, status: true, conn: 'CNAME', address: res});
-                            } else {
-                                results.push({domain: domain.domain, status: false, conn: 'CNAME', error: 'resolvedInvalid'});
-                            }
-                        } catch (e) {
-                            results.push({domain: domain.domain, status: false, conn: 'CNAME', error: 'resolverError'});
-                        }
-                        break;
-                    } 
-                    default: {
-                        results.push({domain: domain.domain, status: false, error: 'invalidConnType'});
-                    }
+                    break;
+                } 
+                default: {
+                    results.push({domain: domain.domain, status: false, error: 'invalidConnType'});
                 }
             }
         }
@@ -71,48 +61,37 @@ if (process.env.NEXT_PUBLIC_ENV === 'development') {
         print(domains);
         let results = []; 
         for (const domain of domains) {
-            const onCloudflare = await dig.resolveNs(domain.domain);
-            print(onCloudflare);
-            if (onCloudflare[0].includes('ns.cloudflare.com') || onCloudflare[0].includes('cloudflare.net')) {
-                const x = await dig.resolve4(domain.domain);
-                const res = await axios.get(`https://${x[0]}/_nslookup/trace`, { headers: { host: domain.domain } });
-                print(res);
-                if (res.data.status) {
-                    results.push({domain: domain.domain, status: true, conn: 'A', addresses: ['This domain is correctly pointing to mlinx\'s servers via Cloudflare\'s proxy.']});
-                }
-            } else {
-                try {
-                    const res = await axios.get(`https://api.vercel.com/v9/projects/${process.env.VERCEL_PROJECT_ID}/domains/${domain.domain}`, {
-                        headers: {
-                            Authorization: `Bearer ${process.env.VERCEL_TOKEN}`
-                        }
-                    });
-                    print(res);
-                    const { verified, verification } = res.data;
-                    if (verified) {
-                        if (!domain.domain.split('.')[2]) {
-                            const res = await dig.resolve4(domain.domain);
-                            print(res);
-                            if (/^76\.76\.21\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(res[0])) {
-                                results.push({domain: domain.domain, status: true, conn: 'A', addresses: res});
-                            } else {
-                                results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolvedInvalid'});
-                            }
-                        } else { 
-                            const res = await dig.resolveCname(domain.domain);
-                            print(res);
-                            if (/^cname\.vercel-dns\.com$/.test(res[0])) {
-                                results.push({domain: domain.domain, status: true, conn: 'CNAME', address: res});
-                            } else {
-                                results.push({domain: domain.domain, status: false, error: 'resolvedInvalid'});
-                            }
-                        }
-                    } else {
-                        results.push({domain: domain.domain, status: false, error: 'unverified', verification: verification[0]});
+            try {
+                const res = await axios.get(`https://api.vercel.com/v9/projects/${process.env.VERCEL_PROJECT_ID}/domains/${domain.domain}`, {
+                    headers: {
+                        Authorization: `Bearer ${process.env.VERCEL_TOKEN}`
                     }
-                } catch (e) {
-                    results.push({domain: domain.domain, status: false, error: 'apiError'});
+                });
+                print(res);
+                const { verified, verification } = res.data;
+                if (verified) {
+                    if (!domain.domain.split('.')[2]) {
+                        const res = await dig.resolve4(domain.domain);
+                        print(res);
+                        if (/^76\.76\.21\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(res[0])) {
+                            results.push({domain: domain.domain, status: true, conn: 'A', addresses: res});
+                        } else {
+                            results.push({domain: domain.domain, status: false, conn: 'A', error: 'resolvedInvalid'});
+                        }
+                    } else { 
+                        const res = await dig.resolveCname(domain.domain);
+                        print(res);
+                        if (/^cname\.vercel-dns\.com$/.test(res[0])) {
+                            results.push({domain: domain.domain, status: true, conn: 'CNAME', address: res});
+                        } else {
+                            results.push({domain: domain.domain, status: false, error: 'resolvedInvalid'});
+                        }
+                    }
+                } else {
+                    results.push({domain: domain.domain, status: false, error: 'unverified', verification: verification[0]});
                 }
+            } catch (e) {
+                results.push({domain: domain.domain, status: false, error: 'apiError'});
             }
         }
         print(results);
