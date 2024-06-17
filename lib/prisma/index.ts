c78ca5a6234263d631d93f6089b68b90a23a70a3
mrtin42 <me@martin.blue>
2024-06-17 08:25:11 +0100

    remove prisma edge

--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -1,9 +1,5 @@
 import { PrismaClient as serverClient } from '@prisma/client'
-import { PrismaClient as edgingClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const ormServer = new serverClient()
-const ormEdge = new edgingClient().$extends(withAccelerate())
 
 export default ormServer;
-export { ormEdge };
